8413914f9c1b11f44507fe00081edf66c499984e
Asdasd <42079569+tragogol@users.noreply.github.com>
2022-12-09 23:23:42 +0300

    Balance change

--- src/main/java/com/zato/randomWebProject/controller/BalanceController.java
+++ src/main/java/com/zato/randomWebProject/controller/BalanceController.java
@@ -1,13 +1,22 @@
 package com.zato.randomWebProject.controller;
 
+import com.zato.randomWebProject.data.Balance;
 import com.zato.randomWebProject.data.Users;
 import com.zato.randomWebProject.repository.BalanceRepository;
 import com.zato.randomWebProject.service.BalanceService;
 import com.zato.randomWebProject.service.UserService;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.security.auth.login.LoginException;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class BalanceController {
@@ -22,7 +31,7 @@ public class BalanceController {
     this.userService = userService;
   }
 
-  @PostMapping("/balance/create_balance")
+  @GetMapping("/balance/create_balance")
   public ResponseEntity<?> createBalance(){
     Users user = userService.findSelfUser();
     if (balanceService.getBalance(user) == null) {
@@ -30,6 +39,17 @@ public class BalanceController {
       return ResponseEntity.status(HttpStatus.OK).body("Balance created");
     } else {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You already have one");
+    }
+  }
+
+  @GetMapping("/balance")
+  public EntityModel<Balance> getBalance() throws LoginException {
+    try {
+      Balance tmpBalance = balanceService.getBalance(userService.findSelfUser());
+      return EntityModel.of(tmpBalance,
+              linkTo(methodOn(BalanceController.class).getBalance()).withSelfRel());
+    } catch (IllegalArgumentException tmpBalance) {
+      throw new LoginException();
     }
   }
 }
