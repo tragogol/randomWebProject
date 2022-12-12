5a4e70a990235e1913bccebcf74fedce8fe27016
Asdasd <42079569+tragogol@users.noreply.github.com>
2022-12-12 21:04:46 +0300

    added find single request method

--- src/main/java/com/zato/randomWebProject/repository/ProductRequestRepository.java
+++ src/main/java/com/zato/randomWebProject/repository/ProductRequestRepository.java
@@ -3,12 +3,14 @@ package com.zato.randomWebProject.repository;
 import com.zato.randomWebProject.data.Product;
 import com.zato.randomWebProject.data.ProductRequest;
 import com.zato.randomWebProject.data.Users;
+import org.h2.engine.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRequestRepository extends JpaRepository<ProductRequest, Long> {
-  ProductRequest findBySeller(Users user);
+  ProductRequest findBySellerAndProductAndQuantityAndPrice(Users seller, Product product, Long quantity, Double price);
+  List<ProductRequest> findBySeller(Users user);
   List<ProductRequest> findByProductAndPriceOrderByQuantity(Product product, Double price);
   List<ProductRequest> findByProductAndQuantityAndPrice(Product product, Long quantity, Double price);
 }
